a82a4c50d6ed6219c7562b2c1d2dc0794b299a28
Ijemma Onwuzulike <ijemmaonwuzulike10@gmail.com>
2018-05-24 17:55:45 -0400

    temp - updated routes to return back simple messages

--- src/router.js
+++ src/router.js
@@ -64,13 +64,21 @@ router.route('/randomUser')
     UserController.randomUser(req, res)
   })
 
-router.route('/reminders')
-  .post(Reminders.createReminder) // create reminder, pass in info
-  .get(Reminders.getReminders); // get all of user's reminders, pass in user
-
 router.route('/reminders/:id')
-  .get(Reminders.getReminder) // get a single reminder using id
-  .put(Reminders.updateReminder) // update a single reminder using id and fields
+  .get((req, res) => {
+    // gets all user reminders
+    res.send({ message: 'all user\'s reminders' })
+  })
+  // .post(Reminders.createReminder) // create reminder, pass in info
+  // .get(Reminders.getReminders); // get all of user's reminders, pass in user
+
+router.route('/reminders/:id&:type')
+  .get((req, res) => {
+    // gets a specific user reminder
+    res.send({ message: 'specific user reminder' })
+  })
+  // .get(Reminders.getReminder) // get a single reminder using id
+  // .put(Reminders.updateReminder) // update a single reminder using id and fields
 
 router.route('/progress/:id')
   .get((req, res) => {
